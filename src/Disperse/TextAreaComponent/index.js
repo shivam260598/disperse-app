d27951a54ee703e86e4e0f71f18458732ae3238b
shivam260598 <46588584+shivam260598@users.noreply.github.com>
2023-09-28 18:36:24 +0000

    css fixes

--- src/Disperse/TextAreaComponent/index.js
+++ src/Disperse/TextAreaComponent/index.js
@@ -5,11 +5,7 @@ import GLOBAL_CONSTANTS from '../../commons/global';
 function TextAreaComponent({ amounts = '', setAmounts = () => {} }) {
     const lineElRef = useRef();
     const textRef = useRef();
-    const textContainerRef = useRef();
     const [line, setLine] = useState(GLOBAL_CONSTANTS.one);
-    const [activeLine, setActiveLine] = useState(GLOBAL_CONSTANTS.one);
-
-    useEffect(() => { handleScroller(); }, []);
 
     const handleScroller = () => {
         let element = textRef?.current || null;
@@ -18,10 +14,12 @@ function TextAreaComponent({ amounts = '', setAmounts = () => {} }) {
             element.onscroll = (e) => {
                 const bottomReached = (element.scrollTop + element.offsetHeight) >= (element.scrollHeight);
                 if (bottomReached) lineElRef.current.scrollTop = element.scrollTop;
-                lineElRef.current.scrollTop = element.scrollTop
+                lineElRef.current.scrollTop = element.scrollTop;
             };
         };
     };
+
+    useEffect(() => { handleScroller(); }, []);
 
     const textChangeHandler = (e) => {
         const val = e.target.value;
@@ -29,26 +27,18 @@ function TextAreaComponent({ amounts = '', setAmounts = () => {} }) {
         if(newLine > 0) setLine(newLine + 1);
         if(newLine === 0) setLine(1);
 
-        activeLineChangeHandler(e);
         setAmounts(val);
     }
 
-    const activeLineChangeHandler = (e) => {
-        const activeLineVal = e.target.value.substr(0, e.target.selectionStart)
-            .split(GLOBAL_CONSTANTS.new_line_escape_sequence).length;
-        if(activeLine  !== activeLineVal) setActiveLine(activeLineVal);
-    }
-
     const renderNumberOfLines = () => {
-        return [...Array(line).keys()].map(i => <span key={i} >{i+1}</span>);
+        return [...Array(line).keys()].map(i => <span key={i}>{i + 1}</span>);
     };
 
     return (
         <div className="text_area_container">
-            <div className="text_container" ref={textContainerRef}>
+            <div className="text_container">
                 <div className="line_number" ref={lineElRef}>{renderNumberOfLines()}</div>
                 <textarea 
-                    onMouseUp={activeLineChangeHandler} 
                     ref={textRef}
                     onChange={textChangeHandler}
                     value={amounts}
